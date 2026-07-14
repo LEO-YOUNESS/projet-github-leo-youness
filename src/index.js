563a7f139f4c0c9627fcc1bd58d1e708bf4ea1eb
Youness <youneessnb@hotmail.fr>
2026-07-14 15:09:21 +0200

    ajout import

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import { Maison } from "./Maison.js";
+import { Piece } from "./Piece.js";
+import { Cave } from "./Cave.js";
+import { Appareil } from "./Appareil.js";
+import { CoffreFort } from "./CoffreFort.js";
